f7a4a097157fee65e4554097edf9b12e9ca41603
Ahmed Shariff <shariff.mfa@outlook.com>
2021-10-22 18:33:16 -0500

    Completed plugin with shortcut

--- background.js
+++ background.js
@@ -1,31 +1,70 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    var filename = tab.title.replace(/\.pdf$/, "") + ".pdf"; 
-    chrome.downloads.download({
-        url: tab.url,
-        filename: filename,
-        conflictAction: 'overwrite'
-    }, function (downloadId) {
-        // chrome.downloads.search({id: downloadId}, function (downloadItem) {
-        //     console.log("filename with absolute local path " , downloadItem.filename);
-        // });
+// Button to trigger download
+
+// Save the current tab as a file
+function downloadCopy() {
+    chrome.tabs.query({
+	currentWindow: true,
+	active: true
+    }, function(tab) {
+	// var filename = tab.title.replace(/\.pdf$/, "") + ".pdf"; 
+        chrome.downloads.download({
+            url: tab[0].url,
+            // filename: filename
+            // conflictAction: 'overwrite'
+        }, function (_downloadId) {
+            //
+        });
     });
-    copyURL(filename);
-});
+}
 
-// chrome.downloads.onChanged.addListener(function (e) {
-//     if (typeof e.state !== 'undefined') {
-//         console.log('Download ID ' + e.id + 'boop' + e.filename);
-// 	if (e.state.current === 'complete') {
-// 	    console.log('Download ID ' + e.id + ' has completed  ' + e.filename);
-// 	}
-//     }
-// });
-
-function copyURL(url) {
+// copy provided string to the clipboard
+function copyString(str) {
     var textArea = document.createElement("textarea");
-    textArea.value = url;
+    textArea.value = str;
     document.body.appendChild(textArea);
     textArea.select();
     document.execCommand("copy");
     textArea.remove();
 }
+
+// The renaming url function
+function renameURLToInstitute(){
+    chrome.tabs.query({
+	currentWindow: true,
+	active: true
+    }, function(tab) {
+        var splitURL = tab[0].url.split("/");
+        splitURL.forEach(function(val, i){
+	    if (val.endsWith(".org") || val.endsWith(".com")) {
+    	        val = val.replace(".", "-") + ".uml.idm.oclc.org";
+            }
+            splitStr[i] = val;
+        });
+        url = splitURL.join("/");
+        
+        updateProperties = new Object();
+        updateProperties.url = url;
+        chrome.tabs.update(tab[0], updateProperties);
+    });
+}
+
+chrome.browserAction.onClicked.addListener(downloadCopy);
+chrome.browserAction.onClicked.addListener(renameURLToInstitute);
+
+// This allows any pdf file downloaded to be copied out
+chrome.downloads.onChanged.addListener(function (e) {
+    if (typeof e.filename !== 'undefined') {
+	console.log("detected file being downloaded", e.filename);
+        if (e.filename.current.endsWith(".pdf")) {
+            copyString(e.filename.current);
+        }
+    }
+    console.log("An event happened for downloads.onChanged");
+    console.log(e);
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+    if (command == 'download-copy') {
+	downloadCopy();
+    }
+});
