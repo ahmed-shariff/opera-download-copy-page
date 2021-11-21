c3a916cedb75319dd95aff8201fdf672d83891b5
Ahmed Shariff <shariff.mfa@outlook.com>
2021-11-20 21:15:23 -0600

    Using org-protocol to send url and pdf file to emacs

--- background.js
+++ background.js
@@ -1,14 +1,18 @@
 // Button to trigger download
 
+var downloadUrl = null;
+var fileName = null;
+
 // Save the current tab as a file
 function downloadCopy() {
     chrome.tabs.query({
 	currentWindow: true,
 	active: true
     }, function(tab) {
-	// var filename = tab.title.replace(/\.pdf$/, "") + ".pdf"; 
+	// var filename = tab.title.replace(/\.pdf$/, "") + ".pdf";
+        downloadUrl = tab[0].url;
         chrome.downloads.download({
-            url: tab[0].url,
+            url: downloadUrl,
             // filename: filename
             // conflictAction: 'overwrite'
         }, function (_downloadId) {
@@ -29,13 +33,33 @@ function copyString(str) {
 
 chrome.browserAction.onClicked.addListener(downloadCopy);
 
-// This allows any pdf file downloaded to be copied out
+// This allows any pdf file downloaded to be copied out and trigger org-protocol
 chrome.downloads.onChanged.addListener(function (e) {
+    if (downloadUrl == null)
+        return;
+
     if (typeof e.filename !== 'undefined') {
 	console.log("detected file being downloaded", e.filename);
         if (e.filename.current.endsWith(".pdf")) {
-            copyString(e.filename.current);
+            fileName = e.filename.current;
         }
+        else if (downloadUrl.includes("ieeexplore.ieee.org/stamp/stamp.jsp")) {
+            chrome.tabs.query({
+	    currentWindow: true,
+	    active: true
+            }, function(tab) {
+                chrome.tabs.executeScript({file: "redirectIEEE.js"});
+            });
+        }
+    }
+
+    if (fileName != null && "state" in e && e.state.current === 'complete') {
+        const orgProtocolHref = `org-protocol:///add-doi-pdf?url=${downloadUrl}&filename=${fileName}`;
+        console.log(`Opening: ${orgProtocolHref}`);
+        window.open(orgProtocolHref);
+        copyString(orgProtocolHref);
+        downloadUrl = null;
+        fileName = null;
     }
 });
 
